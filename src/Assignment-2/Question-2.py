0a2d8cefbed657d32717f536a927fe9435926425
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2023-11-15 17:48:08 +0530

    I added a code of function

--- src/Assignment-2/Question-2.py
+++ src/Assignment-2/Question-2.py
@@ -1,3 +1,10 @@
 print ("this is 2nd question")
 print ("GIT Question")
 print ("Changes some lines")
+def greet():
+    print('Hello World!')
+
+# call the function
+greet()
+
+print('Outside function')
